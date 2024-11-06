5409fdaa5f77da66015b33c777748ef429039e71
C-o-m-o-n <comon928@gmail.com>
2024-11-06 17:42:45 +0300

    start styling playscreen

--- app/(tabs)/[id]/index.tsx
+++ app/(tabs)/[id]/index.tsx
@@ -1,11 +1,22 @@
-import { useRouter, useLocalSearchParams  } from "expo-router";
+import { useRouter, useLocalSearchParams, router } from "expo-router";
 import { useEffect, useState } from "react";
-import { View, Text, Button } from "react-native";
+import {
+  View,
+  Text,
+  Button,
+  Touchable,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 import * as MediaLibrary from "expo-media-library";
 import { Audio } from "expo-av";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Ionicons from "@expo/vector-icons/build/Ionicons";
+import { FontAwesome, FontAwesome6 } from "@expo/vector-icons";
 
 export default function PlayScreen() {
-  const { id } = useLocalSearchParams ();
+  const { id } = useLocalSearchParams();
   const [audioFile, setAudioFile] = useState<MediaLibrary.Asset | null>(null);
   const [sound, setSound] = useState<Audio.Sound | null>(null);
   const [audioFiles, setAudioFiles] = useState<MediaLibrary.Asset[]>([]);
@@ -23,7 +34,7 @@ export default function PlayScreen() {
 
   useEffect(() => {
     if (id && audioFiles.length > 0) {
-      const index = audioFiles.findIndex(file => file.id === id);
+      const index = audioFiles.findIndex((file) => file.id === id);
       if (index !== -1) {
         setCurrentIndex(index);
         playSound(audioFiles[index].uri);
@@ -70,7 +81,6 @@ export default function PlayScreen() {
           handleNext(); // Automatically play next when current track ends
         }
       });
-
     } catch (error) {
       console.error("Error playing audio:", error);
     }
@@ -103,15 +113,95 @@ export default function PlayScreen() {
     }
   };
 
+  const bgImageUri = {
+    uri: "https://images.unsplash.com/photo-1730541843784-09aceb8a1b63?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyM3x8fGVufDB8fHx8fA%3D%3D",
+  };
+  // const bgImageUri = {uri: "https://images.unsplash.com/photo-1727915325711-5fdfb5a0a55c?q=80&w=2096&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
   return (
-    <View className="flex-1 h-full pt-12">
+    <View className="h-full">
       {audioFiles.length > 0 && currentIndex !== -1 ? (
-        <>
-          <Text>{audioFiles[currentIndex].filename}</Text>
-          <Button title={isPlaying ? "Pause" : "Play"} onPress={handlePauseResume} />
-          <Button title="Previous" onPress={handlePrevious} disabled={currentIndex === 0} />
-          <Button title="Next" onPress={handleNext} disabled={currentIndex === audioFiles.length - 1} />
-        </>
+        <ImageBackground
+          source={bgImageUri}
+          resizeMode="cover"
+          className="flex-1 pt-6 justify-between"
+        >
+          <View className="flex flex-row items-center justify-between px-4">
+            <TouchableOpacity onPress={() => router.back()}>
+              <AntDesign
+                className="p-2 rounded-full bg-black/50"
+                name="left"
+                size={24}
+                color="white"
+              />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => router.back()}>
+              <MaterialIcons
+                className="p-2 rounded-full bg-black/50"
+                name="more-vert"
+                size={24}
+                color="white"
+              />
+            </TouchableOpacity>
+          </View>
+
+          <View>
+            <Text className="text-white">
+              {audioFiles[currentIndex].filename}
+            </Text>
+            <Text className="text-white">
+              {audioFiles[currentIndex].filename}
+            </Text>
+            <Text className="text-white">Progress bar</Text>
+
+            <View className="flex flex-row items-center justify-between px-12">
+              <FontAwesome name="random" size={24} color="white" />
+
+              <View className="flex flex-row items-center justify-between gap-2">
+              <Ionicons
+                onPress={handlePrevious}
+                disabled={currentIndex === 0}
+                name="play-skip-back"
+                size={24}
+                color="white"
+              />
+              {isPlaying ? (
+                <TouchableOpacity onPress={handlePauseResume}>
+                  <Ionicons
+                    className="p-4 bg-[#FFAE00] rounded-full items-center"
+                    name="pause"
+                    size={24}
+                    color="white"
+                  />
+                </TouchableOpacity>
+              ) : (
+                <TouchableOpacity onPress={handlePauseResume}>
+                  <Ionicons
+                    className="p-4 bg-[#FFAE00] rounded-full items-center"
+                    name="play"
+                    size={24}
+                    color="white"
+                  />
+                </TouchableOpacity>
+              )}
+
+              <Ionicons
+                onPress={handleNext}
+                disabled={currentIndex === audioFiles.length - 1}
+                name="play-skip-forward"
+                size={24}
+                color="white"
+              />
+              </View>
+
+              <FontAwesome6 name="repeat" size={24} color="white" />
+            </View>
+
+            <View className="flex flex-row justify-between items-center p-2">
+            <AntDesign name="sharealt" size={24} color="white" />
+            <Ionicons name="list" size={24} color="white" />
+            </View>
+          </View>
+        </ImageBackground>
       ) : (
         <Text>Loading...</Text>
       )}
