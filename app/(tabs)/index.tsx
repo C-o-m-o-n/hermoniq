3246ead435dff5b5a2365d3dae9c6520db4645c7
C-o-m-o-n <comon928@gmail.com>
2024-11-06 10:59:30 +0300

    add playing sound

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,6 +9,7 @@ import {
   View,
   Platform,
   StatusBar,
+  TouchableOpacity,
 } from "react-native";
 import * as MediaLibrary from "expo-media-library";
 import { HelloWave } from "@/components/HelloWave";
@@ -19,27 +20,56 @@ import { Audio } from "expo-av";
 
 export default function HomeScreen() {
   const [sound, setSound] = useState<Audio.Sound | undefined>(undefined);
+  const [audioFiles, setAudioFiles] = useState<MediaLibrary.Asset[]>([]);
+  const [currentSong, setCurrentSong] = useState(false);
+  
 
   useEffect(() => {
     return sound
       ? () => {
-          console.log('Unloading Sound');
+          console.log("Unloading Sound");
           sound.unloadAsync();
         }
       : undefined;
   }, [sound]);
 
-
-  async function playSound() {
+  async function playSound(uri: string) {
     console.log("Loading Sound");
-    const { sound } = await Audio.Sound.createAsync( require("../../assets/audio/soulsweeper.mp3")
-    );
-    setSound(sound);
-
-    console.log('Playing Sound');
-    await sound.playAsync();
+    const sound = new Audio.Sound();
+    try {
+      await sound.loadAsync({ uri });
+      await sound.playAsync();
+    } catch (error) {
+      console.error('Error playing audio:', error);
+    }
   }
 
+  const requestPermissions = async () => {
+    const { status } = await MediaLibrary.requestPermissionsAsync();
+    if (status !== "granted") {
+      alert("Permission to access media library is required!");
+      return false;
+    }
+    return true;
+  };
+
+  useEffect(() => {
+    requestPermissions();
+    loadAudioFiles();
+  }, []);
+
+  const loadAudioFiles = async () => {
+    try {
+      const media = await MediaLibrary.getAssetsAsync({
+        mediaType: MediaLibrary.MediaType.audio,
+        first: 100, // Number of files to fetch
+      });
+
+      setAudioFiles(media.assets);
+    } catch (error) {
+      console.error("Error fetching audio files:", error);
+    }
+  };
 
   return (
     <SafeAreaView
@@ -47,9 +77,17 @@ export default function HomeScreen() {
         Platform.OS === "android" ? StatusBar.currentHeight : 10
       }`}
     >
-      <ScrollView className="flex-1">
+      <ScrollView className="flex-1 pt-6">
         <ThemedText>Home</ThemedText>
-        <Button title="Play Sound" onPress={playSound} />
+
+        <View className="bg-red-400 p-2">
+          {audioFiles.map((file, index) => (
+            <TouchableOpacity onPress={() => playSound(file.uri)} className="py-4" key={index}>
+              <ThemedText>{file.filename}</ThemedText>
+            </TouchableOpacity>
+          ))}
+        </View>
+
       </ScrollView>
     </SafeAreaView>
   );
